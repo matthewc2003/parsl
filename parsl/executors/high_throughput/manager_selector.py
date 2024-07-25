853a291c46210189b9fcdebe232ed0f7763a8e73
matthewc2003 <matthewc2003@gmail.com>
2024-07-25 20:10:26 +0000

    Added another manager selector which sorts by block_id of the managers, from least to greatest. Also includes accompanying test

--- parsl/executors/high_throughput/manager_selector.py
+++ parsl/executors/high_throughput/manager_selector.py
@@ -20,3 +20,16 @@ class ManagerSelectorRandom(ManagerSelectorBase):
         c_manager_list = copy.copy(manager_list)
         random.shuffle(c_manager_list)
         return c_manager_list
+
+
+class ManagerSelectorByBlockId(ManagerSelectorBase):
+
+    def __init_(self):
+        pass
+
+    def sort_managers(self, ready_managers: Dict[bytes, ManagerRecord], manager_list: List[bytes]) -> List[bytes]:
+        def get_block_id(m: bytes) -> str:
+            temp = ready_managers[m]['block_id']
+            return temp if temp is not None else ""
+
+        return sorted(copy.copy(manager_list), key=get_block_id, reverse=True)
